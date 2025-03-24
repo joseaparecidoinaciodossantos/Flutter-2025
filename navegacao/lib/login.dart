7aeabcf23b33efde5fe8280898b285b40980f998
joseaparecidoinaciodossantos <joseaparecidoinacio2022@gmail.com>
2025-03-24 17:27:03 -0300

    alteração na tela de login

--- navegacao/lib/login.dart
+++ navegacao/lib/login.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main(){
   runApp(Preconfiguracao());
@@ -25,13 +28,52 @@ class Login extends StatefulWidget{
   }
 
 
-
+@override
 class LoginEstado extends State<Login>{
   final emailControle = TextEditingController();
  final senhaControle = TextEditingController();
-  bool estaCarregado = false;
+  bool estaCarregando = false;
   String mensagemErro = '';
   bool ocultado = true;
+
+  Future<void> logar() async{
+    setState(() {
+
+    //inicia animação de carregamento
+    estaCarregando = true;
+    mensagemErro = '';
+  });
+
+  final url = Uri.parse('https://aplicativotransito-5710d-default-rtdb.firebaseio.com/usuario.json');
+  final resposta = await http.get(url);
+  //se tudo estiver certo...override
+    // TODO: implement ==
+    if(resposta.statusCode== 200){
+      final Map<String, dynamic>? dados = jsonDecode(resposta.body);
+
+if(dados != null){
+   bool usuarioValido = false;
+   String nomeUsuario = '';
+
+      dados.forEach(key, valor){
+        if(valor['email'] == emailControle.text && valor['senha'] == senhaControle.text){
+          usuarioValido == true;
+          nomeUsuario = valor['usuario'];
+        }
+      });
+    }
+    //se o usuario for valido, ou seja ,se tiver no banco
+    if(usuarioValido = true){
+      Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) =>Apliactivo));
+    }
+
+}
+    }else{
+      setState(() { mensagemErro = 'Erro de conexão'; });
+        
+      }
+}
+       
   @override
 
   Widget build(BuildContext context) {
@@ -78,11 +120,11 @@ class LoginEstado extends State<Login>{
             ),
           ),
           SizedBox(height: 30,),
-          estaCarregado ? CircularProgressIndicator() :
+          estaCarregando ? CircularProgressIndicator() :
           ElevatedButton(onPressed: null, child: Text('Entrar')),
-          SizedBox(height: 30,),
-          TextButton(onPressed: null, child: Text('Não tem uma conta? Cadastre-se'),),
-
+         TextButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Cadastro()));
+              }, child: Text('Não tem uma conta? cadastre-se'),),    
         ]
     ),
     ),
@@ -90,3 +132,106 @@ class LoginEstado extends State<Login>{
   }
 
 }
+
+class Cadastro extends StatefulWidget {
+  @override
+  CadastroEstado createState() => CadastroEstado();
+}
+
+class CadastroEstado extends State<Cadastro> {
+  final nomeControle = TextEditingController();
+  final emailControle = TextEditingController();
+  final senhaControle = TextEditingController();
+  bool estaCarregando = false;
+  String erro='';
+  bool ocultado = true;
+
+  Future<void> cadastrar() async{
+    //iniciar carregamento
+    setState(() { estaCarregando = true; });
+
+    final nome = nomeControle.text;
+    final email = emailControle.text;
+    final senha = senhaControle.text;
+
+    final url = Uri.parse('https://aplicativotransito-5710d-default-rtdb.firebaseio.com/usuario.json');
+    final resposta = await http.post(
+      url,
+      body:jsonEncode({
+        'nome' : nome,
+        'email' : email,
+        'senha' : senha
+      }),
+      headers:{'Content-Type' : 'application/json'},
+    );
+    if(resposta.statusCode == 200){
+      Navigator.pop(context); 
+    }else{
+      setState(() {
+        erro = "erro ao cadastrar usúario";
+      });
+      setState(() {
+        estaCarregando = false;
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Cadastro de novo usuario'),
+      backgroundColor: Colors.blue,),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+         child: Column(
+          children: [
+               Icon(Icons.person_pin, size: 100, color: Colors.blue,),
+            SizedBox(height: 20,),
+            TextField(
+              controller: emailControle,
+              decoration: InputDecoration(
+                 labelText:'email',
+                 border: OutlineInputBorder(),
+               prefixIcon: Icon(Icons.email),
+               prefixIconColor: Colors.blue, 
+               ),
+            ),
+            SizedBox(height: 20,),
+             TextField(
+              controller: nomeControle,
+              decoration: InputDecoration(
+                labelText:'Seu nome',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.person),
+                prefixIconColor:Colors.blue,   
+               ),
+            ),
+
+            SizedBox(height: 20,),
+               TextField(
+                controller:senhaControle,
+                obscureText: ocultado,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  prefixIconColor: Colors.blue,
+                  suffixIcon: IconButton(
+                    icon: Icon(ocultado ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {setState(() {ocultado = !ocultado;
+                      });
+                    },            
+                  ),
+                ),
+               ),
+              SizedBox(height: 30,),
+             estaCarregando ? CircularProgressIndicator() : ElevatedButton(
+              onPressed: cadastrar, child: Text('Cadastrar'),
+              ),
+              erro.isNotEmpty ? Text(erro, style: TextStyle(color:Colors.red)): SizedBox(),
+          ],
+         ),
+         ),
+    ); 
+  }
+}
+
